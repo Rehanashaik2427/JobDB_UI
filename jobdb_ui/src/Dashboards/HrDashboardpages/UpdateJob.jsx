382befa6aa0db309f7a0e62c484ea832a93d3de5
rehanashaik2427 <shaikrehana2427@gmail.com>
2024-06-21 11:03:31 +0530

    commit update job

--- jobdb_ui/src/Dashboards/HrDashboardpages/UpdateJob.jsx
+++ jobdb_ui/src/Dashboards/HrDashboardpages/UpdateJob.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
 import { FaEdit, FaSave } from "react-icons/fa";
 import { useLocation, useNavigate } from 'react-router-dom';
 import HrLeftSide from "./HrLeftSide";
@@ -7,47 +8,49 @@ import HrLeftSide from "./HrLeftSide";
 const UpdateJob = () => {
   const BASE_API_URL = "http://localhost:8082/api/jobbox";
   const location = useLocation();
-  const navigate=useNavigate();
-  
+  const { userName, userEmail, jobId } = location.state || {};
+
+
+
+  const navigate = useNavigate();
   
   const [editableJobDetails, setEditableJobDetails] = useState(false);
   const [jobDetails, setJobDetails] = useState({});
-  const { jobId, userName, userEmail } = location.state || {};
+  const [formData, setFormData] = useState({
+    jobTitle: '',
+    jobType: '',
+    location: '',
+    salary: '',
+    postingDate: '',
+    qualifications: '',
+    applicationDeadline: '',
+    numberOfPositions: '',
+    jobSummary: '',
+    skills: '',
+  });
 
-  console.log(location.state);
 
-  console.log(userEmail,userName,jobId);
-  
-  
+
   useEffect(() => {
     if (jobId) {
       fetchJobDetails(jobId);
     }
   }, [jobId]);
 
-  const fetchJobDetails = async (jobId) => {
+  const fetchJobDetails = async (id) => {
     try {
-      const response = await axios.get(`${BASE_API_URL}/getJob?jobId=${jobId}`,);
+      const response = await axios.get(`${BASE_API_URL}/getJob`, { params: { jobId: id } });
       setJobDetails(response.data);
       setFormData(response.data);
     } catch (error) {
       console.error('Error fetching job details:', error);
     }
   };
-  const [formData, setFormData] = useState({
-    jobTitle: '',
-    jobType: '',
-    location: '',
-    salary: '',
-    postingdate: '',
-    qualifications: '',
-    applicationDeadline: '',
-    positions: '',
-    postingDate: '',
-    jobsummary: ''
-  });
 
-
+  const user = {
+    userName: userName,
+    userEmail: userEmail,
+  };
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -65,134 +68,128 @@ const UpdateJob = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      await axios.put(`${BASE_API_URL}/updateJob?jobId=${jobId}`,formData); // Assuming your endpoint for updating jobs is /updateJob
+      await axios.put(`${BASE_API_URL}/updateJob?jobId=${jobId}`, formData);
       alert('Job details updated successfully.');
-      navigate({
-        pathname: '/post-jobs',
+      navigate('/hr-dashboard/my-jobs', {
         state: {
           userName: userName,
           userEmail: userEmail,
-          
-        }
+        },
       });
-
     } catch (error) {
       console.error('Error updating job details:', error);
     }
   };
 
-  const user = {
-    userName: userName,
-    userEmail: userEmail,
-  };
-
   return (
-    <div className='hr-dashboard-container'>
-      <div className="hr-leftside">
-        <HrLeftSide user={user} />
-      </div>
+    <Container fluid className="hr-dashboard-container">
+    <Row>
+      <Col md={3} className="hr-leftside">
+        <HrLeftSide user={{ userName, userEmail }} />
+      </Col>
 
-      <div className='hr-rightside'>
-        <form className="job-posting-form" onSubmit={handleSubmit}>
-          <div>
-            <h2 style={{ textDecoration: 'underline' }}>Job Details </h2>
-            <div className='job-details-row'>
-              <div className='job-form-group'>
-                <label htmlFor="jobTitle">Job Title:</label>
-                <input
-                  type="text"
-                  id="jobTitle"
-                  name="jobTitle"
-                  value={formData.jobTitle}
-                  onChange={handleChange}
-                  disabled={!editableJobDetails}
-                />
-              </div>
-              <div className='job-form-group'>
-                <label htmlFor="jobType">Job Type:</label>
-                <input
-                  type="text"
-                  id="jobType"
-                  name="jobType"
-                  value={formData.jobType}
-                  onChange={handleChange}
-                  disabled={!editableJobDetails}
-                />
-              </div>
-              <div className='job-form-group'>
-                <label htmlFor="postingdate">Posting Date:</label>
-                <input
-                  type="date"
-                  id="postingdate"
-                  name="postingDate"
-                  value={formData.postingDate}
-                  onChange={handleChange}
-                  disabled={!editableJobDetails}
-                />
-              </div>
+      <Col md={9} className="hr-rightside">
+      <form className="job-posting-form" onSubmit={handleSubmit}>
+        <div>
+          <h2 style={{ textDecoration: 'underline' }}>Job Details </h2>
+          <div className='job-details-row'>
+            <div className='job-form-group'>
+              <label htmlFor="jobTitle">Job Title:</label>
+              <input
+                type="text"
+                id="jobTitle"
+                name="jobTitle"
+                value={formData.jobTitle}
+                onChange={handleChange}
+                disabled={!editableJobDetails}
+              />
             </div>
-            <div className='job-details-row'>
-              <div className='job-form-group'>
-                <label htmlFor="skills">Skills:</label>
-                <input
-                  type="text"
-                  id="skills"
-                  name="skills"
-                  value={formData.skills}
-                  onChange={handleChange}
-                  disabled={!editableJobDetails}
-                />
-              </div>
-              <div className='job-form-group'>
-                <label htmlFor="positions">Number of Positions:</label>
-                <input
-                  type="number"
-                  id="positions"
-                  name="numberOfPosition"
-                  value={formData.numberOfPosition}
-                  onChange={handleChange}
-                  disabled={!editableJobDetails}                />
-              </div>
-              <div className='job-form-group'>
-                <label htmlFor="deadline">Application Deadline:</label>
-                <input
-                  type="date"
-                  id="deadline"
-                  name="applicationDeadline"
-                  value={formData.applicationDeadline}
-                  onChange={handleChange}
-                  disabled={!editableJobDetails}
-                />
-              </div>
-              <div className='job-form-group'>
-                <label htmlFor="jobsummary">Job summary: Add Additional Information</label>
-                <textarea
-                  id="jobsummary"
-                  name="jobsummary"
-                  value={formData.jobsummary}
-                  onChange={handleChange}
-                  className="fullWidthTextarea"
-                  disabled={!editableJobDetails}
-                />
-              </div>
+            <div className='job-form-group'>
+              <label htmlFor="jobType">Job Type:</label>
+              <input
+                type="text"
+                id="jobType"
+                name="jobType"
+                value={formData.jobType}
+                onChange={handleChange}
+                disabled={!editableJobDetails}
+              />
             </div>
-            <div className='job-save-edit-buttons'>
-            
-              {editableJobDetails ? (
-                <button type="button" onClick={handleSaveJobDetails}><FaSave />Save</button>
-              ) : (
-                <button type="button" onClick={handleEditJobDetails}><FaEdit />Edit</button>
-              )}
-               <button type="submit">
-              Post
-
-               </button>
+            <div className='job-form-group'>
+              <label htmlFor="postingdate">Posting Date:</label>
+              <input
+                type="date"
+                id="postingdate"
+                name="postingDate"
+                value={formData.postingDate}
+                onChange={handleChange}
+                disabled={!editableJobDetails}
+              />
             </div>
-           
           </div>
-        </form>
-      </div>
-    </div>
+          <div className='job-details-row'>
+            <div className='job-form-group'>
+              <label htmlFor="skills">Skills:</label>
+              <input
+                type="text"
+                id="skills"
+                name="skills"
+                value={formData.skills}
+                onChange={handleChange}
+                disabled={!editableJobDetails}
+              />
+            </div>
+            <div className='job-form-group'>
+              <label htmlFor="positions">Number of Positions:</label>
+              <input
+                type="number"
+                id="positions"
+                name="numberOfPosition"
+                value={formData.numberOfPosition}
+                onChange={handleChange}
+                disabled={!editableJobDetails}                />
+            </div>
+            <div className='job-form-group'>
+              <label htmlFor="deadline">Application Deadline:</label>
+              <input
+                type="date"
+                id="deadline"
+                name="applicationDeadline"
+                value={formData.applicationDeadline}
+                onChange={handleChange}
+                disabled={!editableJobDetails}
+              />
+            </div>
+            <div className='job-form-group'>
+              <label htmlFor="jobsummary">Job summary: Add Additional Information</label>
+              <textarea
+                id="jobsummary"
+                name="jobsummary"
+                value={formData.jobsummary}
+                onChange={handleChange}
+                className="fullWidthTextarea"
+                disabled={!editableJobDetails}
+              />
+            </div>
+          </div>
+          <div className='job-save-edit-buttons'>
+          
+            {editableJobDetails ? (
+              <button type="button" onClick={handleSaveJobDetails}><FaSave />Save</button>
+            ) : (
+              <button type="button" onClick={handleEditJobDetails}><FaEdit />Edit</button>
+            )}
+             <button type="submit">
+            Post
+
+             </button>
+          </div>
+         
+        </div>
+      </form>
+</Col>
+</Row>
+</Container>
   );
 };
 
