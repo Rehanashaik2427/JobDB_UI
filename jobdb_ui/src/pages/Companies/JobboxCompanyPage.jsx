1e1af80e342e0da5c27f8eb15decbe52408837b7
rehanashaik2427 <shaikrehana2427@gmail.com>
2024-08-05 18:28:39 +0530

    commit jobboxcompanypage page number

--- jobdb_ui/src/pages/Companies/JobboxCompanyPage.jsx
+++ jobdb_ui/src/pages/Companies/JobboxCompanyPage.jsx
@@ -9,7 +9,7 @@ const JobboxCompanyPage = () => {
   const [companies, setCompanies] = useState([]);
   const [search, setSearch] = useState('');
   const [page, setPage] = useState(0);
-  const [pageSize, setPageSize] = useState(3);
+  const [pageSize, setPageSize] = useState(6);
   const [totalPages, setTotalPages] = useState(0);
   const navigate = useNavigate();
 
@@ -131,7 +131,7 @@ const JobboxCompanyPage = () => {
         <div className="page-size-select me-3">
             <label htmlFor="pageSize">Page Size:</label>
             <select id="pageSize" onChange={handlePageSizeChange} value={pageSize} disabled={isPageSizeDisabled}>
-              <option value="5">5</option>
+              <option value="6">6</option>
               <option value="10">10</option>
               <option value="20">20</option>
             </select>
